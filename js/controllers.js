d77adf14fe34344e15d3a415945cbf772fa32a18
PortWatcher <root@pwhack.me>
2014-07-31 20:06:35 +0800

    add deviceready event handler

--- js/controllers.js
+++ js/controllers.js
@@ -1,35 +1,37 @@
 angular.module('lydiagapdemo.controllers', [])
 
 .controller('homeCtrl', function ($scope) {
-  $scope.vibrate = function () {
-    navigator.notification.vibrate();
-  };
+  document.addEventListener('deviceready', function () {
+    $scope.vibrate = function () {
+      navigator.notification.vibrate();
+    };
 
-  var statusbar = {};
-  statusbar.hidden = !StatusBar.isVisible;
-  statusbar.toggle = function () {
-    if (!statusbar.hidden) {
-      statusbar.hidden = true;
-      StatusBar.hide();
-    } else {
-      statusbar.hidden = false;
-      StatusBar.show();
-    }
-  };
-  $scope.statusbar = statusbar;
-
-  $scope.accl = {
-    x: '未获取',
-    y: '未获取',
-    z: '未获取'
-  };
-  $scope.getAccl = function () {
-    navigator.accelerometer.getCurrentAcceleration(function (accl) {
-      $scope.accl = accl;
-    }, function (err) {
-      if (err) {
-        alert(err.msg);
+    var statusbar = {};
+    statusbar.hidden = !StatusBar.isVisible;
+    statusbar.toggle = function () {
+      if (!statusbar.hidden) {
+        statusbar.hidden = true;
+        StatusBar.hide();
+      } else {
+        statusbar.hidden = false;
+        StatusBar.show();
       }
-    });
-  };
+    };
+    $scope.statusbar = statusbar;
+
+    $scope.accl = {
+      x: '未获取',
+      y: '未获取',
+      z: '未获取'
+    };
+    $scope.getAccl = function () {
+      navigator.accelerometer.getCurrentAcceleration(function (accl) {
+        $scope.accl = accl;
+      }, function (err) {
+        if (err) {
+          alert(err.msg);
+        }
+      });
+    };
+  });
 });
