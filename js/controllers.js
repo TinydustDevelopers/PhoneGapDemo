8380d222554a352068d3e7ace3493f0c34ba382f
PortWatcher <root@pwhack.me>
2014-08-13 23:29:37 +0800

    add socialsharing api

--- js/controllers.js
+++ js/controllers.js
@@ -43,6 +43,8 @@ angular.module('lydiagapdemo.controllers', [])
         $scope.$apply();
       }, function (err) {
         console.log(err.message);
+        accelerometer.listening = false;
+        accelerometer.disable();
       });
     };
 
@@ -61,5 +63,37 @@ angular.module('lydiagapdemo.controllers', [])
     };
 
     $scope.accelerometer = accelerometer;
+
+    var social = {};
+
+    social.shareWechat = function () {
+      window.plugins.socialsharing.shareViaWeChat('告诉你一个秘密', 'Hello, world', location.href, function () {
+        console.log('分享成功');
+      }, function (err) {
+        console.log(err.message);
+      });
+    };
+
+    social.shareTimeLine = function () {
+      window.plugins.socialsharing.shareViaWeChatTimeLine('我特么也能击败全国99%的人', '就是title么，谁不会', location.href, function () {
+        console.log('分享成功');
+      }, function (err) {
+        console.log(err.message);
+      });
+    };
+
+    social.shareWeibo = function () {
+      window.plugins.socialsharing.shareViaWeibo('微博标题', '微博内容', location.href, function () {
+        console.log('分享成功');
+      }, function (err) {
+        console.log(err.message);
+      });
+    };
+
+    social.shareThis = function () {
+      window.plugins.socialsharing.shareCurrentPage();
+    };
+
+    $scope.social = social;
   });
 });
