28ebb7916256fb743ec59a3366ae705581882be5
PortWatcher <root@pwhack.me>
2014-08-30 00:37:57 +0800

    add notification

--- js/controllers.js
+++ js/controllers.js
@@ -76,5 +76,24 @@ angular.module('lydiagapdemo.controllers', [])
     };
 
     $scope.social = social;
+
+    $scope.notifyMe = function () {
+      if (!(Notification in window)) {
+        alert('请使用1.0或以上版本来打开此web app以获得消息通知功能');
+      }
+
+      Notification.requestPermission(function (status) {
+        if (status === 'granted') {
+          var notification = new Notification('Hi there!', {
+            icon: 'http://cdn.lydiabox.com/images/1407001318444.png',
+            body: 'I\'m LydiaBox'
+          });
+
+          setTimeout(function () {
+            notification.close();
+          }, 2000);
+        }
+      });
+    };
   });
 });
